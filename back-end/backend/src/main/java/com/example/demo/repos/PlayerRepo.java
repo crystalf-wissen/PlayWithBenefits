bbda92b2155118801526e62725ea0f43506fa11e
Sanat Patil <sanat.patil@wissen.com>
2025-04-10 17:26:08 +0530

    Removed backned folder

--- back-end/backend/src/main/java/com/example/demo/repos/PlayerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.Player;
-
-public interface PlayerRepo extends JpaRepository<Player, Integer>{
-
-}
